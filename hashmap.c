953633c794a55d1c66f50bd64de8ca6258db3619
Your Name <you@example.com>
2025-04-16 09:57:56 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -82,10 +82,27 @@ HashMap * createMap(long capacity) {
 void eraseMap(HashMap * map,char* key){ 
 
 }
+/*
+3.- Implemente la función Pair * searchMap(HashMap * map,  char * key), la cual retorna el **Pair** asociado a la clave ingresada. 
+Recuerde que para buscar el par debe:
+a - Usar la función hash para obtener la posición donde puede encontrarse el par con la clave
 
+b - Si la clave no se encuentra avance hasta encontrarla (*método de resolución de colisiones*)
+
+c - Si llega a una casilla nula, retorne NULL inmediatamente (no siga avanzando, la clave no está)
+
+Recuerde actualizar el índice current a la posición encontrada.
+Recuerde que el arreglo es **circular**.
+
+    Pair * searchMap(HashMap * map,  char * key) {
+        return NULL;
+    }
+*/
 Pair * searchMap(HashMap * map,  char * key) {   
-
-
+    pos = hash(key,map->capacity);
+    while(map->buckets[pos] != NULL){
+        if(is_equal(map->buckets[pos]->key,key)) return map->buckets[pos];
+    }
     return NULL;
 }
 
