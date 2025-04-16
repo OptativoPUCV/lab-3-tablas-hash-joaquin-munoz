7bca441ec79792dda8bec17246434057287c7a6b
Your Name <you@example.com>
2025-04-16 09:39:32 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -38,10 +38,27 @@ int is_equal(void* key1, void* key2){
     return 0;
 }
 
+/*2.- Implemente la función void insertMap(HashMap * map, char * key, void * value). Esta función inserta un nuevo dato (key,value) en el mapa y
+ actualiza el índice current a esa posición. Recuerde que para insertar un par (clave,valor) debe:
+ a - Aplicar la función hash a la clave para obtener la posición donde debería insertar el nuevo par
 
+b - Si la casilla se encuentra ocupada, avance hasta una casilla disponible (*método de resolución de colisiones*).
+ Una casilla disponible es una casilla nula, pero también una que tenga un par inválido (key==NULL).
+
+c - Ingrese el par en la casilla que encontró.
+
+No inserte claves repetidas. 
+Recuerde que el arreglo es **circular**.
+Recuerde actualizar la variable size.
+*/
 void insertMap(HashMap * map, char * key, void * value) {
-
-
+    long posicion = hash(key,map->capacity);
+    while(map->buckets[posicion] != NULL || map->buckets[posicion]->key != NULL){
+        posicion = (posicion + 1) % map->capacity;
+    }
+    Pair * par = createPair(strdup(key),value);
+    map->size++;
+    map->buckets[posicion] = par;
 }
 
 void enlarge(HashMap * map) {
