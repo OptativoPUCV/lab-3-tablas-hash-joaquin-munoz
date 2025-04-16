92c8b9389c67783ded5d73668e8172c1d4b95b4c
Your Name <you@example.com>
2025-04-16 10:05:39 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -105,6 +105,7 @@ Pair * searchMap(HashMap * map,  char * key) {
             map->current = pos;
             return map->buckets[pos];
         }
+        pos = (pos + 1) % map->capacity;
     }
     return NULL;
 }
